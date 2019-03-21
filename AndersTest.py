b1e210967221352c5cf5e0111082579b321a8a85
Anders S. J <ajoha14@student.aau.dk>
2019-03-21 14:07:14 +0100

    Removed numpy from buffer class

--- AndersTest.py
+++ AndersTest.py
@@ -1,8 +1,31 @@
 from Signals.hrProc import hrProcesser
+from misc.buffer import Buffer
+import matplotlib.pyplot as plt
+import numpy as np
 
-c = hrProcesser(1,10)
-l = c.heartRate(['03_19-11_00_39.100000','03_19-11_00_40.100000','03_19-11_00_41.100000','03_19-11_00_42.100000','03_19-11_00_43.100000','03_19-11_00_44.100000'])
+c = hrProcesser()
+buffer = Buffer()
+with open("C:/Users/Anders S. Johansen/Desktop/data2.txt") as f:
+    d = f.read().splitlines()
 
-for i in range(100):
-    print('' + str(c.hrbuffer.data) + '\r')
-    c.hrbuffer.add(i)
+#SETUP LIVE PLOTTER
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+for i in range(len(d)):
+    a = np.array(d[i].split(','))
+    buffer.add(a)
+    print(buffer.data[0][1])
+    if len(buffer.data) == buffer.size:
+        
+        #Do calculations
+        l = c.ampd(buffer.data[0][:]) #Peak Detectoin
+        
+        #Update plot
+        ax.clear()
+        ax.plot(buffer.data[:][0])
+        ax.scatter(l,buffer.data[l][0])
+        plt.show()
+        plt.pause(0.001)
+
