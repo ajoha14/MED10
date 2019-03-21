d104149f550245c787f225f953626193a7cd3517
Anders S. J <ajoha14@student.aau.dk>
2019-03-21 14:05:32 +0100

    Fixed Buffer for dynamic scaling dimensions

--- misc/buffer.py
+++ misc/buffer.py
@@ -1,15 +1,15 @@
 import numpy as np
 class Buffer:
     def __init__(self, sz=5):
-        self.data = np.array([])
+        self.data = []
         self.size = sz
 
     def add(self, lmnt):
         if len(self.data) >= self.size:
             self.data = self.data[1:]
-            self.data = np.append(self.data, lmnt)
+            self.data.append(lmnt)
         else:
-            self.data = np.append(self.data, lmnt)
+            self.data.append(lmnt)
 
     def removeOldest(self):
         self.data = self.data[1:]
