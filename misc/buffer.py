86f4af444b99b4541e2efc4bc2527d9836182a74
Jesper Wædeled Henriksen <jesper.waedeled.henriksen@gmail.com>
2019-03-21 14:28:06 +0100

    Added isFull to buffer class

--- misc/buffer.py
+++ misc/buffer.py
@@ -3,14 +3,19 @@ class Buffer:
         self.data = []
         self.size = size
 
-    def add(self, lmnt):
-        if len(self.data) >= self.size:
+    def add(self, newData):
+        if self.isFull():
             self.data = self.data[1:]
-            self.data.append(lmnt)
+            self.data.append(newData)
         else:
-            self.data.append(lmnt)
+            self.data.append(newData)
 
     def removeOldest(self):
         self.data = self.data[1:]
-    
+
+    def isFull(self):
+        if len(self.data) >= self.size:
+            return True
+        else:
+            return False
 
