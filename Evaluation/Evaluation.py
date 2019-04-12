c3f897b55a471711a3d32179a5ebe80738df1c06
Jesper Wædeled Henriksen <jesper.waedeled.henriksen@gmail.com>
2019-04-12 14:04:18 +0200

    Evaluation scripts Almost Done

--- Evaluation/Evaluation.py
+++ Evaluation/Evaluation.py
@@ -1,6 +1,12 @@
 import csv
 import numpy as np
 
+numberOfTasks = 1
+numberOfInitialQuestions = 4
+numberOfQuestionPerTasks = 15
+datapointsPerParticipants = numberOfQuestionPerTasks * numberOfTasks + numberOfInitialQuestions;
+
+
 def getDataFromFile(file, seperator = ','):
     data = []
     print("Opening file: '" + str(file) + "'")
@@ -15,26 +21,24 @@ def getDataFromFile(file, seperator = ','):
 
 #{ } [ ]
 def convertDataToMatrix(data):
-    def extractTaskData(data, begin, end):
-        temp = []
-        for x in range(begin, end):
-            temp.append(data[1][x])
-        return temp
-
     rows = len(data[:]); cols = len(data[0])
     print("rows,cols = [" + str(rows)+","+str(cols) + "]")
+    print(datapointsPerParticipants)
 
-    #listOfMatrixes = [ ]
-    #dataMatrix = np.zeros((rows,cols))
-    #dataMatrix = [[0 for x in range(9)] ,[0 for y in range(cols)]]
-    dataMatrix = [[] ,[]]
-    dataMatrix[0].append("Initial");    dataMatrix[0].append("Task 1")
-    dataMatrix[0].append("Task 2");      dataMatrix[0].append("Task 3");
-    dataMatrix[0].append("Task 7");    dataMatrix[0].append("Task 5");
-    dataMatrix[0].append("Task 6");    dataMatrix[0].append(" Task 7");
-    dataMatrix[0].append("Task 8");
-    #Initial
-    dataMatrix[1].append(extractTaskData(data,1,5))
-    #Task
-    dataMatrix[1].append(extractTaskData(data,5,5+8))
-    print(dataMatrix)
+    p = Participant()
+    p.loadData(data,1)
+    p.loadData(data,1)
+    #p.loadData(data,1,1)
+
+    print(p.dataMatrix)
+
+class Participant:
+    def __init__(self):
+        self.dataMatrix = []
+
+
+    def loadData(self,data,start):
+        print(data)
+        self.dataMatrix.append(data[1][start:start + numberOfInitialQuestions])
+        #line below should iterate through each task and append for each task. Then it is done
+        self.dataMatrix.append(data[1][start + numberOfInitialQuestions:start + numberOfInitialQuestions + numberOfQuestionPerTasks])
