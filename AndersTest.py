c183659c21690c0d8baa9268360d8cd796fad2e4
Anders S. J <ajoha14@student.aau.dk>
2019-04-30 09:13:10 +0200

    Removed Comments

--- AndersTest.py
+++ AndersTest.py
@@ -23,93 +23,13 @@ def testEyeTrack():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         rects = detector(gray,1)
 
-        for (i,rect) in enumerate(rects):
+        for (i,face) in enumerate(rects):
             #Get facial landmark points
-            shape = predictor(gray, rect)
+            shape = predictor(gray, face)
             shape = shape_to_np(shape) #36-41(left eye), 42-47(right eye)
             l_eye_p = shape[36:42]
             r_eye_p = shape[42:48]
-            lbb = bounding_box(l_eye_p)
-            rbb = bounding_box(r_eye_p)
-            #Calculate bounding box width & Height
-            l_width = lbb[1][0] - lbb[0][0]
-            r_width = rbb[1][0] - rbb[0][0]
-            l_height= lbb[1][1] - lbb[0][1]
-            r_height= rbb[1][1] - rbb[0][1]
-            #Calculate Center
-            l_center = (int(lbb[0][0] + l_width/2), int(lbb[0][1] + (l_height/2)))
-            r_center = (int(rbb[0][0] + l_width/2), int(rbb[0][1] + (r_height/2)))
-            #Cropout eye
-            l_eye = gray[l_center[1]-int(l_width/2):l_center[1]+int(l_width/2), l_center[0]-int(l_width/2):l_center[0]+int(l_width/2)]
-            r_eye = gray[r_center[1]-int(r_width/2):r_center[1]+int(r_width/2), r_center[0]-int(r_width/2):r_center[0]+int(r_width/2)]
-            #Create Mask
-            im_mask = np.zeros(frame.shape, dtype=np.uint8)
-            im_mask = cv2.fillPoly(im_mask, [l_eye_p],(255,255,255))
-            im_mask = cv2.fillPoly(im_mask, [r_eye_p],(255,255,255))
-            im_mask = cv2.cvtColor(im_mask, cv2.COLOR_BGR2GRAY)
-            l_mask = im_mask[l_center[1]-int(l_width/2):l_center[1]+int(l_width/2), l_center[0]-int(l_width/2):l_center[0]+int(l_width/2)]
-            r_mask = im_mask[r_center[1]-int(r_width/2):r_center[1]+int(r_width/2), r_center[0]-int(r_width/2):r_center[0]+int(r_width/2)]
-            #Apply Mask
-            left_eye = frame[l_center[1]-int(l_width/2):l_center[1]+int(l_width/2), l_center[0]-int(l_width/2):l_center[0]+int(l_width/2)]
-            right_eye = frame[r_center[1]-int(r_width/2):r_center[1]+int(r_width/2), r_center[0]-int(r_width/2):r_center[0]+int(r_width/2)]
-            left_eye = cv2.bitwise_and(left_eye, left_eye, mask=l_mask)
-            right_eye = cv2.bitwise_and(right_eye, right_eye, mask=r_mask)
-            #Left edges
-            l_edges = cv2.cvtColor(left_eye, cv2.COLOR_BGR2GRAY)
-            r_edges = cv2.cvtColor(right_eye, cv2.COLOR_BGR2GRAY)
-            #l_edges = cv2.medianBlur(l_edges, 7)
-            #r_edges = cv2.medianBlur(r_edges, 7)
-            l_edges = cv2.equalizeHist(l_edges)
-            r_edges = cv2.equalizeHist(r_edges)
-            l_edges = cv2.GaussianBlur(l_edges,(7,7),0)
-            r_edges = cv2.GaussianBlur(r_edges,(7,7),0)
-            l_edges = cv2.adaptiveThreshold(l_edges,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,13,1)
-            r_edges = cv2.adaptiveThreshold(r_edges,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,13,1)
-            #Crop away eye lid edges
-            l_edges = cv2.bitwise_and(l_edges, l_edges, mask=l_mask)
-            r_edges = cv2.bitwise_and(r_edges, r_edges, mask=r_mask)
-            #Hit and fit
-            kernel = np.ones((2,2),np.uint8)
-            l_edges = cv2.erode(l_edges, kernel, iterations=1)
-            r_edges = cv2.erode(r_edges, kernel, iterations=1)
-            l_edges = cv2.dilate(l_edges, kernel, iterations=1)
-            r_edges = cv2.dilate(r_edges, kernel, iterations=1)
-            
-            #Houghcircles
-            l_circ = cv2.HoughCircles(l_edges,cv2.HOUGH_GRADIENT,2,5,param1=30,param2=25,minRadius=0,maxRadius=int(l_width/3))
-            r_circ = cv2.HoughCircles(r_edges,cv2.HOUGH_GRADIENT,2,5,param1=30,param2=25,minRadius=0,maxRadius=int(r_width/3))
-            l_circ = np.uint16(np.around(l_circ))
-            r_circ = np.uint16(np.around(r_circ))
-
-            i_l = l_circ[0,0]
-            r_l = l_circ[0,0]
-            cv2.circle(left_eye,(i_l[0],i_l[1]),2,(0,255,0),2)
-
-            #Draw left Circles
-            #for i in l_circ[0,:]:
-                #print(i)
-                #draw the outer circle
-                #cv2.circle(left_eye,(i[0],i[1]),i[2],(0,255,0),2)
-                # draw the center of the circle
-                #cv2.circle(left_eye,(i[0],i[1]),2,(0,0,255),3)
-            #for i in r_circ[0,:]:
-                #print(i)
-                # draw the outer circle
-                #cv2.circle(right_eye,(i[0],i[1]),i[2],(0,255,0),2)
-                # draw the center of the circle
-                #cv2.circle(right_eye,(i[0],i[1]),2,(0,0,255),3)
-
-
-
-        #Show Images
-        cv2.circle(frame, l_center, 1, (255,0,0),1)
-        cv2.circle(frame, r_center, 1, (255,0,0),1)
-        cv2.imshow("full frame",frame)
-        cv2.imshow("left eye", np.hstack((left_eye,cv2.cvtColor(l_edges,cv2.COLOR_GRAY2BGR))))
-        cv2.imshow("right eye", np.hstack((right_eye, cv2.cvtColor(r_edges,cv2.COLOR_GRAY2BGR))))
-        cv2.waitKey(1)
-        if 0xFF == ord('q'):
-            break
+            segmentEye(l_eye_p, frame)
     #Cleanup
     cap.release()
     cv2.destroyAllWindows()
@@ -148,6 +68,57 @@ def testhr():
             plt.show()
             plt.pause(0.01)
 
+def segmentEye(points, image, min_size=10):
+    #Get bounding box
+    bb = bounding_box(points)
+    width = bb[1][0] - bb[0][0]
+    height = bb[1][1] - bb[0][1]
+    #Ignore small bounding boxes
+    if width < min_size:
+        print("eye too small")
+        return 0.0
+    #Convert image to Greyscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #Create Mask
+    mask = np.zeros(image.shape, dtype=np.uint8)
+    mask = cv2.fillPoly(mask, [points],(255))
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    #Apply mask
+    gray = cv2.bitwise_and(gray, gray, mask=mask)
+    #Crop ROI
+    roi = gray[bb[0][1]:bb[1][1], bb[0][0]:bb[1][0]]
+    #Histogram equalizatoin (improve contrast)
+    blob = cv2.equalizeHist(roi)
+    #Blur (to remove noise)
+    blob = cv2.medianBlur(blob,5)
+    blob = cv2.GaussianBlur(blob, (3,3), 0)
+    #Adaptive Threshold / gradient edge detection
+    blob = cv2.adaptiveThreshold(blob,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,5,1)
+    #Remove Eyelid gradient
+    eye_mask = mask[bb[0][1]:bb[1][1], bb[0][0]:bb[1][0]]
+    blob = cv2.bitwise_and(blob, blob, mask=eye_mask)
+    #Hit And Fit
+    kernel = np.ones((3,3),np.uint8)
+    blob = cv2.erode(blob, kernel, iterations=1)
+    #blob = cv2.dilate(blob, kernel, iterations=2)
+    #Hough Circles
+    circles = cv2.HoughCircles(blob, cv2.HOUGH_GRADIENT, 2, 5, param1=45, param2=20, minRadius=int(width/8), maxRadius=int(width/2))
+    #Ignore if no iris detected
+    if circles is None:
+        print("Couldnt find Iris")
+        return 0.0
+    circles = np.uint16(np.around(circles))
+    best_circle = circles[0,0]
+    #Iris Location is at the center
+    print(best_circle)
+    print(roi.shape, blob.shape)
+    #Debug
+    print("eye found")
+    #cv2.circle(roi, (best_circle[0],best_circle[1]), 2, (255,255,255), 2)
+    cv2.circle(roi, (best_circle[0],best_circle[1]), best_circle[2], (255,255,255), 2)
+    cv2.imshow("eye", np.hstack((roi,blob)))
+    cv2.waitKey(1)
+
 def rect_to_bb(rect):
     x = rect.left()
     y = rect.top()
@@ -176,7 +147,6 @@ def bounding_box(points):
 def auto_canny(image, sigma=0.30):
 	# compute the median of the single channel pixel intensities
 	v = np.median(image)
-
 	# apply automatic Canny edge detection using the computed median
 	lower = int(max(0, (1.0 - sigma) * v))
 	upper = int(min(255, (1.0 + sigma) * v))
@@ -185,15 +155,10 @@ def auto_canny(image, sigma=0.30):
 	# return the edged image
 	return edged
 
-def iris_segment(image):
-    #Using daugmans method
-
-    #Convert image to greyscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
-
-    return image
-
+def pupil_ratio(image):
+    x_gradient = image[:][best_circle[0]]
+    y_gradient = image[best_circle[1]][:]
+    print(x_gradient)
+    print(y_gradient)
 
 testEyeTrack()
